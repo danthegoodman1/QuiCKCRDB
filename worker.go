a186324b0a3487842003dd7ce0d2b576325206e6
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-03-30 13:27:19 -0400

    scanner goroutine and stop function

--- worker.go
+++ worker.go
@@ -3,13 +3,19 @@ package quickcrdb
 import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"runtime"
+	"sync/atomic"
 	"time"
 )
 
 type (
 	Worker struct {
-		pool   *pgxpool.Pool
-		config *workerConfig
+		pool         *pgxpool.Pool
+		config       *workerConfig
+		hashRingSize int
+		stopScanner  chan any
+		stopManagers chan any
+		stopWorkers  chan any
+		shuttingDown *atomic.Bool
 	}
 	workerConfig struct {
 		managerRoutines      int
@@ -44,21 +50,41 @@ var (
 	}
 )
 
-func NewWorker(pool *pgxpool.Pool, opts ...WorkerOption) (*Worker, error) {
+func NewWorker(pool *pgxpool.Pool, hashRingSize int, opts ...WorkerOption) (*Worker, error) {
 	worker := &Worker{
-		pool:   pool,
-		config: defaultConfig,
+		pool:         pool,
+		config:       defaultConfig,
+		hashRingSize: hashRingSize,
+		shuttingDown: &atomic.Bool{},
 	}
 
 	for _, opt := range opts {
 		opt(worker.config)
 	}
 
+	worker.stopScanner = make(chan any, 1)
+	worker.stopManagers = make(chan any, worker.config.managerRoutines)
+	worker.stopWorkers = make(chan any, worker.config.workerRoutines)
+
 	return worker, nil
 }
 
 func (w *Worker) scanner() {
+	token := 0
+	for {
+		select {
+		case <-w.stopScanner:
+			logger.Info().Msg("scanner exiting")
+			return
+		default:
+			// TODO: Scan hash token for queue zones
+		}
 
+		token++
+		if token > w.hashRingSize {
+			token = 0
+		}
+	}
 }
 
 func (w *Worker) manager() {
@@ -67,4 +93,17 @@ func (w *Worker) manager() {
 
 func (w *Worker) worker() {
 
+}
+
+// StopScanner tells the scanner goroutine. It is safe to crash all goroutines, so on exit you don't even need to stop
+func (w *Worker) StopScanner() {
+	if w.shuttingDown.CompareAndSwap(false, true) {
+		w.stopScanner <- nil
+		for i := 0; i < w.config.managerRoutines; i++ {
+			w.stopManagers <- nil
+		}
+		for i := 0; i < w.config.workerRoutines; i++ {
+			w.stopWorkers <- nil
+		}
+	}
 }
